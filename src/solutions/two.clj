ff1aa50efd16947b9f0159dafdc772cb14ce6454
Gustave Rousselet <gustave.rousselet@icloud.com>
2022-12-04 15:00:19 +0000

    Format day 2

--- src/solutions/two.clj
+++ src/solutions/two.clj
@@ -11,17 +11,17 @@
 (defn solve [input]
   (let [commands (input->commands input)]
     {:p1 (let [{h :horizonal d :depth} (->> commands
-                           (reduce (fn [acc {c :command v :val}]
-                                     (case c
-                                       "forward" (update acc :horizonal + v)
-                                       "down" (update acc :depth + v)
-                                       "up" (update acc :depth - v))) {:horizonal 0 :depth 0}))]
+                                            (reduce (fn [acc {c :command v :val}]
+                                                      (case c
+                                                        "forward" (update acc :horizonal + v)
+                                                        "down" (update acc :depth + v)
+                                                        "up" (update acc :depth - v))) {:horizonal 0 :depth 0}))]
            (* h d))
      :p2 (let [{h :horizonal d :depth} (->> commands
-                           (reduce (fn [{aim :aim :as acc} {c :command v :val}]
-                                     (case c
-                                       "forward" (-> (update acc :horizonal + v)
-                                                     (update :depth (fn [d] (+ d (* v aim)))))
-                                       "down" (update acc :aim + v)
-                                       "up" (update acc :aim - v))) {:horizonal 0 :depth 0 :aim 0}))]
+                                            (reduce (fn [{aim :aim :as acc} {c :command v :val}]
+                                                      (case c
+                                                        "forward" (-> (update acc :horizonal + v)
+                                                                      (update :depth (fn [d] (+ d (* v aim)))))
+                                                        "down" (update acc :aim + v)
+                                                        "up" (update acc :aim - v))) {:horizonal 0 :depth 0 :aim 0}))]
            (* h d))}))
